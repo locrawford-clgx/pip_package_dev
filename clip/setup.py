3468c70ba5dce033786bfb5632f6277556388ef7
locrawford-clgx <locrawford@corelogic.com>
2023-06-12 13:15:30 -0500

    Install configuration changes

--- clip/setup.py
+++ clip/setup.py
@@ -2,23 +2,22 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name = 'Clip_test_library',
-    url = 'https://github.com/corelogic/saa-geospatial-tools/tree/master/clip',
-    author = 'Logan Crawford',
-    author_email = 'locrawford@corelogic.com',
+    name="clip",
+    url="https://github.com/locrawford-clgx/pip_package_dev/tree/main/clip",
+    author="Logan Crawford",
+    author_email="locrawford@corelogic.com",
     
     # Needed to actually package something
-    packages = ['clip'],
+    packages=["clip"],
     
     # Needed for dependencies
-    install_requires = ['os', 'json', 'requests', 'pandas', 'datetime'],
+    install_requires=["requests", "pandas"],
     
     # *strongly* suggested for sharing
-    version = '1.0.0',
+    version="1.0.0",
     
     # The license can be anything you like
-    
-    license = 'MIT',
-    description = 'An example of a python package from pre-existing code',
-    long_description = open('README.txt').read(),
+    license="MIT",
+    description="An example of a Python package from pre-existing code",
+    long_description=open("README.md").read(),
 )
